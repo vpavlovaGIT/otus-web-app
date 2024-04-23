a108dfa901336c21fe347c1698f67a1e9ef1c364
Victoria Pavlova <viktoriya.n.pavlova@rt.ru>
2024-04-23 10:38:08 +0300

    Реализована обработка более одного потоков.

--- src/main/java/ru/otus/vpavlova/web/app/HttpServer.java
+++ src/main/java/ru/otus/vpavlova/web/app/HttpServer.java
@@ -10,24 +10,43 @@ public class HttpServer {
 
     public HttpServer(int port) {
         this.port = port;
+        this.dispatcher = new Dispatcher();
     }
 
     public void start() {
+        System.out.println("Сервер запущен на порту: " + port);
         try (ServerSocket serverSocket = new ServerSocket(port)) {
-            System.out.println("Сервер запущен на порту: " + port);
-            this.dispatcher = new Dispatcher();
-            System.out.println("Диспетчер проинициализирован");
-            try (Socket socket = serverSocket.accept()) {
-                byte[] buffer = new byte[8192];
-                int n = socket.getInputStream().read(buffer);
-                String rawRequest = new String(buffer, 0, n);
-                HttpRequest request = new HttpRequest(rawRequest);
-                request.info(true);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Установлено соединение с клиентом: " + socket.getInetAddress());
 
-                dispatcher.execute(request, socket.getOutputStream());
+                Thread requestHandlerThread = new Thread(() -> {
+                    try {
+                        byte[] buffer = new byte[8192];
+                        int n = socket.getInputStream().read(buffer);
+                        String rawRequest = new String(buffer, 0, n);
+                        HttpRequest request = new HttpRequest(rawRequest);
+                        request.info(true);
+
+                        dispatcher.execute(request, socket.getOutputStream());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                requestHandlerThread.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        new HttpServer(8189).start();
     }
 }
